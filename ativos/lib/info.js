75420b5bf6a6432a16aecb9407888da93212466d
gabrielalves3391 <77789433+gabrielalves3391@users.noreply.github.com>
2021-01-21 10:07:25 -0400

    Add files via upload

---
+++ ativos/lib/info.js
@@ -0,0 +1,44 @@
+exportações.info = (id, A187, showDate, whatsapp, youtube, showTime, Instagram, número, ativo) => {
+return` 
+❉──────────❉
+
+*${A187}*
+
+❉──────────❉
+
+📆 *${tampilTanggal}*
+📌STATUS BOT ATIVO: *${aktif}*
+
+         
+╔════════════════════
+║ *Feito por: $Dark*
+╠════════════════════
+║   *⚠️ATENÇÃO⚠️*
+╠════════════════════
+║*OS CRÉDITOS NAO SÃO MEUS*
+║*APENAS FIZ UMA VERSÃO INSTÁVEL*
+╠════════════════════
+║╭──❉ *INFO* ❉──
+║│➸ *AUTOR*: Higor
+║│➸ *DESIGNER*: Higor
+║│➸ *YOUTUBE*: Dark ツ
+║╰───────────
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│➸ *Faça login aqui😙*
+║│ _${whatsapp}_
+║│➸ *YouTube*
+║│ _${nomer}_
+║│➸ *Instagram <Segue>*
+║│ _${instagram}_
+║│➸ *Criador*
+║│    Dark ツ
+║╰───────────
+║         ${A187}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${A187}
+╠════════════════════
+║ _*Feito por BOZO66 ツ*_
+╚════════════════════`
+}
---
+++ ativos/lib/menu1.js
@@ -0,0 +1,52 @@
+exports.menu1 = (id, A187, tampilTanggal, whatsapp, youtube, tampilWaktu, instagram, nomer, aktif) => {
+	return ` 
+❉──────────❉
+
+bozo 🤨
+
+❉──────────❉
+
+Bozo66 𝐃𝐎𝐌𝐈𝐍𝐀 🤠🤙 𝐎𝐋𝐇𝐄 𝐎𝐒 𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐀𝐁𝐀𝐈𝐗𝐎
+
+╔════════════════════
+║ 
+╠════════════════════
+║╭──❉ *MEDIAS* ❉──
+║│➸ _*!adesivo*_
+║│➸ _*!foto menina*_
+║│➸ _*!foto *_
+║│➸ _*!pokemon*_ 
+║│➸ _*!loli*_
+║│➸ _*!fotoanime*_
+║│➸ _*! Serviço externo * _
+║│➸ _ *! Ttp * _ <texto>
+║│➸ _ *! Meme * _
+║│➸ _ *! Ssweb * _ <link>
+║╰───────────
+╠════════════════════
+║╭──❉ *EDUCAÇÃO* ❉──
+║│➸ _*!nulo * _ <texto>
+║│➸ _ *! Pantun * _
+║│➸ _ *! Significa nome * _ <seu nome>
+║│➸ _ *! Parceiro * _ <Aris & Cahya>
+║│➸ _ *! Letras * _ <nome da música>
+║│➸ _ *! Chord * _ <nome da música>
+║│➸ _ *! Bucin * _
+║│➸ _ *! Zodiac * _ <28-02-2002>
+║
+║╰───────────
+╠════════════════════
+║╭──❉ * INFORMAÇÕES * ❉──
+║│➸ _ *! Oração * _ <area>
+║│➸ _ *! CovidID * _
+║│➸ _ *! Covidcountry * _ <country>
+║│➸ _ *! Infogempa * _
+║│➸ _ *! Cectdate * _ <Date>
+║│➸ _ *! Infoanime * _ <Naruto>
+║>
+║╰───────────
+
+╠════════════════════
+║ BOZO66 𝐃𝐎𝐌𝐈𝐍𝐀 🤠🤙
+╚════════════════════`
+}
